7fafe62cac8557af0929f387ab54b3d5ccbd0808
Sairaj Rane <112332700+sairajr62@users.noreply.github.com>
2024-07-27 08:42:07 +0000

    Datatypes

---
+++ 02_Datatypes/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 + 3) // code realiability should be high
+
+console.log("Sairaj")
+
+let name = "Sairaj"
+let age = 18
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined =>  Value not Assigned now
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); //object
